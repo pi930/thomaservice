b8f7ee80cb7e602fad0c85672e43fb6c8e268d11
pi930 <t.pierrard.131.198@outlook.fr>
2026-02-12 21:24:33 +0100

    Force HTTPS in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,9 +12,7 @@ class AppServiceProvider extends ServiceProvider
         //
     }
 
-    public function boot(): void
-    {
-        //
-    }
+    public function boot() { if (config('app.env') === 'production') { URL::forceScheme('https'); } 
 }
 
+}
