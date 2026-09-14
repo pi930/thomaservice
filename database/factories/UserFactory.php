46c9060a8c8c5e01742dbba6c24f7f0b793954c7
Thomas <pi930@users.noreply.github.com>
2026-09-14 08:22:31 +0200

    Fix fake() namespace for Laravel 13

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -6,24 +6,14 @@ use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
-use function Laravel\Faker\fake;
-
 
 /**
  * @extends Factory<User>
  */
 class UserFactory extends Factory
 {
-    /**
-     * The current password being used by the factory.
-     */
     protected static ?string $password;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
@@ -35,9 +25,6 @@ class UserFactory extends Factory
         ];
     }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
     public function unverified(): static
     {
         return $this->state(fn (array $attributes) => [
